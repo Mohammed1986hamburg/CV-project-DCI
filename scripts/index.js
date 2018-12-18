644a5b38a4d97a0683d53f3eba404333f164098c
best.pid@gmail.com <best.pid@gmail.com>
2018-12-18 13:09:53 +0100

    first push

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+$().ready(ev => {
+
+
+    $(window).scroll(function () {
+        $(".slideanim").each(function () {
+            var pos = $(this).offset().top;
+
+            var winTop = $(window).scrollTop();
+            if (pos < winTop + 600) {
+                $(this).addClass("slide");
+            }
+        });
+    });
+
+
+
+})
